c4fe39718755d47d556dd024e9bad2dc16d42601
NickolasDzR <nikolyae@mail.ru>
2020-08-09 21:34:11 +0300

    header slider

---
+++ src/blocks/components/main-slider/main-slider.js
@@ -0,0 +1,16 @@
+import Swiper from '../../../../node_modules/swiper/swiper-bundle';
+
+if (document.querySelector('.main-slider')) {
+    const mainSliderContainer = document.querySelector('.main-slider__container');
+
+    const mainSlider = new Swiper(mainSliderContainer, {
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+          pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+        },
+    });
+}
--- src/js/import/components.js
+++ src/js/import/components.js
@@ -2,4 +2,5 @@ import svguse from 'svgxuse';
 import "../../blocks/components/nav/nav";
 import "../../blocks/components/dropdown-menu/dropdown-menu";
 import "../../blocks/components/top-line/top-line";
+import "../../blocks/components/main-slider/main-slider";
 import "../../blocks/components/tabs/tabs";
