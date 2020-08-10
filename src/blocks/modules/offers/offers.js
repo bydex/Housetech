b6972397adf5714614bbacec11625ad35e5e7f10
erok_erok@mail.ru <erok_erok@mail.ru>
2020-08-10 16:53:37 +0600

    offers

---
+++ src/blocks/modules/offers/offers.js
@@ -0,0 +1,13 @@
+modules.define('offers', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
